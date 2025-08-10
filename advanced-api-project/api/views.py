fd99d8b8732b5980ba6b7860530803f207e3b425
ezio <ya817412@gmail.com>
2025-08-10 15:59:55 +0100

    Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,66 @@
 from django.shortcuts import render
+from .models import Author, Book
+from rest_framework import generics, permissions
+from .serializers import BookSerializer
 
 # Create your views here.
+
+class BookListView(generics.ListAPIView):
+    """
+    BookListView:
+    - GET /books/
+    - Returns a list of all books in the database.
+    - Accessible to everyone (authenticated or not).
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+    
+class BookDetailView(generics.RetrieveAPIView):
+    """
+    BookDetailView:
+    - GET /books/<id>/
+    - Returns a single book by its primary key (id).
+    - Accessible to everyone.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+    
+class BookCreateView(generics.CreateAPIView):
+    """
+    BookCreateView:
+    - POST /books/create/
+    - Allows authenticated users to add a new book.
+    - Validates data via BookSerializer.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    def perform_create(self, serializer):
+        """
+        Custom save logic:
+        - Assigns the current logged-in user as the author automatically.
+        """
+        serializer.save(author=self.request.user)
+    
+class BookUpdateView(generics.UpdateAPIView):
+    """
+    BookDeleteView:
+    - DELETE /books/<id>/delete/
+    - Allows authenticated users to delete a book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    def perform_update(self, serializer):
+        """
+        Custom save logic:
+        - Assigns the current logged-in user as the author automatically.
+        """
+        serializer.save(author=self.request.user)
+    
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]

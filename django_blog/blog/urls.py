25d8e24be8b7efc9db4e7743d265c5867338b086
ezio <ya817412@gmail.com>
2025-08-15 19:25:11 +0100

    Adding Comment Functionality to Blog Posts

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/comment/new/', views.CommentCreateView.as_view(), name='comment-create'),
-    path("comment/<int:pk>/edit/", views.CommentUpdateView.as_view(), name="comment-edit"),
+    path("comment/<int:pk>/update/", views.CommentUpdateView.as_view(), name="comment-edit"),
     path("comment/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment-delete"),
 ]
